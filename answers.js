80797ad98613f21d3b6967ee39a46b7eb733b6b1
Lisa Ostman <peolina@live.com.au>
2021-01-06 18:12:33 +1100

    added exercise for students

---
+++ answers.js
@@ -0,0 +1,41 @@
+var assert = require("assert");
+var Mathematics = require("./providing.js");
+// Create a test suite (group) called Math
+describe("Math", function () {
+  // Test One: A string explanation of what we're testing
+  it("should test if 3*3 = 9", function () {
+    // Our actual test: 3*3 SHOULD EQUAL 9
+    assert.equal(9, Mathematics.multiply(3, 3));
+  });
+
+  it("should test that other numbers are also returned", () => {
+    assert.equal(10, Mathematics.multiply(2, 5));
+  });
+
+  it("raises an error if the parameter passed in is not a string", () => {
+    // Setup
+    
+    // Exercise
+    const test = () => {
+     Mathematics.multiply("string", "string")
+    };
+    // Verification
+    assert.throws(test, /only use numbers/);
+  });
+});
+
+
+//----
+
+const Mathematics = {
+    // for Phase 2: insert code here that has key multiply and value of a function that simply returns 9
+    //it should take in two arguments, a and b, but not do anything with them
+
+    //for Phase 4: utilise your two arguments to make the test pass!
+    multiply: function(a, b){
+        if (isNaN(a) || isNaN(b)) {
+            throw new Error("only use numbers");
+          }
+        return a*b
+    }
+};
---
+++ providing.js
@@ -0,0 +1,15 @@
+const Mathematics = {
+    // for Phase 2: insert code here that has key multiply and value of a function that simply returns 9
+    //it should take in two arguments, a and b, but not do anything with them
+
+    //for Phase 4: utilise your two arguments to make the test pass!
+
+
+    //Phase 6:
+    // in the multiply function, create an if statement that checks if a OR b is NaN (not a number!)
+    // if it isn't, then throw a new error with the string inside it that says "ERROR: only use numbers"
+    
+};
+
+//correctly export Mathematics below
+
